9e44d98c3d27492849ab0d43128038c9b66627ef
eliot-portevin <eliotportevin@gmail.com>
2025-01-22 14:33:56 +0100

    Create fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,58 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#
+# Functions
+#
+
+function fish_greeting
+	clear
+	printf "Silent whispers in the night,\nA universe of endless light.\nLife's mysteries unfold with time,\nObserving patterns so sublime.\nMoments fleeting, yet profound,\nEternity in every sound.\n\n- Eirik Lund" | cowsay
+	# colorscript -r
+end
+
+function checkMode
+	set mode (defaults read -g AppleInterfaceStyle)
+
+	if test "$mode" = Dark
+		setTerminalProfile Gruvbox_Dark
+	else
+		setTerminalProfile Solarized_Light
+	end
+end
+
+function setTerminalProfile
+	command osascript -e "tell application \"Terminal\"
+						set default settings to settings set \"$argv\"
+
+						repeat with w in windows
+							repeat with t in tabs of w
+								set current settings of t to settings set \"$argv\"
+							end repeat
+						end repeat
+					end tell"
+end
+
+#
+# Set light/dark mode
+checkMode
+
+#
+# Set aliases
+alias vi "nvim"
+
+setenv -gx EDITOR nvim
+setenv -gx SUDO_EDITOR nvim
+
+export PATH="$PATH:$HOME/.local/bin"
+
+# Setting PATH for Python 3.12
+# The original version is saved in /Users/eliotp/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
+
+# Dotfiles config
+if test -d $HOME/dotfiles
+    alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+end
+
